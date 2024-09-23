2a525442e0541c68d78cbc94210701729d0649b0
Leonardo Caldas <leonardo.ferreira.caldas@gmail.com>
2024-09-23 13:20:05 -0300

    Fix: Added plugin class to composer.json

---
+++ src/ComposableQueryBuilderPlugin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ComposableQueryBuilder;
+
+use Composer\Plugin\PluginInterface;
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\Capable;
+use Composer\Plugin\Capability\CommandProvider;
+
+class ComposableQueryBuilderPlugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        // Plugin activation logic
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        // Optional: Plugin deactivation logic
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+        // Optional: Plugin uninstall logic
+    }
+}
