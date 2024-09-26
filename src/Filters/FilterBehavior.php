de28b0a6f68bbe5b5bff3f2aadb25d6d7c9aa0bf
Leonardo Caldas <leonardo.ferreira.caldas@gmail.com>
2024-09-26 15:38:45 -0300

    change filter order

--- src/Filters/FilterBehavior.php
+++ src/Filters/FilterBehavior.php
@@ -10,28 +10,28 @@ class FilterBehavior
 {
     public static function exactMatch(): callable
     {
-        return function ($columnName, $value, $queryBuilder) {
+        return function ($queryBuilder, $value, $columnName) {
             $queryBuilder->where($columnName, $value);
         };
     }
 
     public static function whereNotNull(): callable
     {
-        return function ($columnName, $value, $queryBuilder) {
+        return function ($queryBuilder, $value, $columnName) {
             $queryBuilder->whereNotNull($columnName, $value);
         };
     }
 
     public static function whereNull(): callable
     {
-        return function ($columnName, $value, $queryBuilder) {
+        return function ($queryBuilder, $value, $columnName) {
             $queryBuilder->whereNull($columnName, $value);
         };
     }
 
     public static function notEquals(): callable
     {
-        return function ($columnName, $value, $queryBuilder) {
+        return function ($queryBuilder, $value, $columnName) {
             $value = str_replace(QueryStringStatementTokens::NOT_EQUALS, '', $value);
 
             $queryBuilder->where($columnName, '!=', $value);
@@ -40,7 +40,7 @@ class FilterBehavior
 
     public static function betweenDateTime(): callable
     {
-        return function ($columnName, $value, $queryBuilder) {
+        return function ($queryBuilder, $value, $columnName) {
             $value = FilterValueExtractor::dateTime($value);
 
             return $queryBuilder->whereBetween($columnName, $value);
@@ -49,14 +49,14 @@ class FilterBehavior
 
     public static function contains(): callable
     {
-        return function ($columnName, $value, $queryBuilder) {
+        return function ($queryBuilder, $value, $columnName) {
             $queryBuilder->where($columnName, 'like', '%' . $value . '%');
         };
     }
 
     public static function fulltext(): callable
     {
-        return function ($columnName, $value, $queryBuilder) {
+        return function ($queryBuilder, $value, $columnName) {
             $queryBuilder->whereRaw(
                 FullTextSearch::normalizeFullTextStatement($columnName, $value)
             );
@@ -65,28 +65,28 @@ class FilterBehavior
 
     public static function startsWith(): callable
     {
-        return function ($columnName, $value, $queryBuilder) {
+        return function ($queryBuilder, $value, $columnName) {
             $queryBuilder->where($columnName, 'like', $value . '%');
         };
     }
 
     public static function endsWith(): callable
     {
-        return function ($columnName, $value, $queryBuilder) {
+        return function ($queryBuilder, $value, $columnName) {
             $queryBuilder->where($columnName, 'like', '%' . $value);
         };
     }
 
     public static function whereIn(): callable
     {
-        return function ($columnName, $value, $queryBuilder) {
+        return function ($queryBuilder, $value, $columnName) {
             $queryBuilder->whereIn($columnName, $value);
         };
     }
 
     public static function numericRange(): callable
     {
-        return function ($columnName, $value, $queryBuilder) {
+        return function ($queryBuilder, $value, $columnName) {
             [$symbolClause, $value] = FilterValueExtractor::range($value);
 
             return $queryBuilder->where($columnName, $symbolClause, $value);
